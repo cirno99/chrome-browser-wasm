0f63fe9e8f8b19c21904ea6d0d99738544b8e19d
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2024-06-22 01:03:17 +0900

    manifest.jsonの修正

--- src/entrypoints/popup/App.vue
+++ src/entrypoints/popup/App.vue
@@ -1,36 +1,41 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import init, { find } from "../../wasm/pkg"
+import { onMounted, ref } from "vue";
+import init, { find } from "../../wasm/pkg";
 
 const targetContent = ref<string>("");
 const query = ref<string>("");
 const searchResult = ref<string[]>([]);
 
 const handleClick = async () => {
-    await init();
-    const result =  find(targetContent.value, query.value);
-    searchResult.value = result;
-}
+  await init();
+
+  const result = find(targetContent.value, query.value);
+  searchResult.value = result;
+};
 
 onMounted(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs[0].id === undefined) return;
-        chrome.tabs.sendMessage<{}, { innerText: string}>(tabs[0].id, {}, (response) => {
-            targetContent.value = response.innerText;
-        });
-    });
-})
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0].id === undefined) return;
+    chrome.tabs.sendMessage<{}, { innerText: string }>(
+      tabs[0].id,
+      {},
+      (response) => {
+        targetContent.value = response.innerText;
+      }
+    );
+  });
+});
 </script>
 
 <template>
-    <div>
-        <h1>Popup!</h1>
+  <div>
+    <h1>Popup!</h1>
 
-        <input v-model="query" type="text" placeholder="Search" />
-        <button @click="handleClick">CLICK</button>
+    <input v-model="query" type="text" placeholder="Search" />
+    <button @click="handleClick">CLICK</button>
 
-        <ul>
-            <li v-for="result in searchResult" :key="result">{{ result }}</li>
-        </ul>
-    </div>
+    <ul>
+      <li v-for="result in searchResult" :key="result">{{ result }}</li>
+    </ul>
+  </div>
 </template>
